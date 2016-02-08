0f73c11c83cdba2e357579b835bfc8e19915c1eb
Chris LaRose <chris.larose@mycase.com>
2016-02-07 22:47:58 -0800

    cjl - Refactor mix

--- src/codewars/mix.clj
+++ src/codewars/mix.clj
@@ -11,19 +11,20 @@
        (into {})))
 
 (defn maximums [s1-freq s2-freq]
-  (for [letter (union (set (keys s1-freq)) (set (keys s2-freq)))]
-    (let [s1-letter (get s1-freq letter 0)
-          s2-letter (get s2-freq letter 0)]
-      (cond
-        (> s1-letter s2-letter)
-          ["1" letter s1-letter]
-        (> s2-letter s1-letter)
-          ["2" letter s2-letter]
-        :else
-          ["=" letter s1-letter]))))
+  (let [fmt (fn [winner letter freq]
+              (str winner ":" (join (repeat freq letter))))]
+    (for [letter (union (set (keys s1-freq)) (set (keys s2-freq)))]
+      (let [s1-letter (get s1-freq letter 0)
+            s2-letter (get s2-freq letter 0)]
+        (cond
+          (> s1-letter s2-letter)
+            (fmt "1" letter s1-letter)
+          (> s2-letter s1-letter)
+            (fmt "2" letter s2-letter)
+          :else
+            (fmt "=" letter s1-letter))))))
 
 (defn mix [s1 s2]
   (->> (maximums (freq-map s1) (freq-map s2))
-       (map (fn [[winner letter freq]] (str winner ":" (join (repeat freq letter)))))
        (sort-by (juxt (comp - count) identity))
        (join "/")))
